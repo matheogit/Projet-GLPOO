50669f00a5fc58e275206c16c86b17ebc24e6682
matheogit <80523951+matheogit@users.noreply.github.com>
2021-03-19 17:58:45 +0100

    pas de bug

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import sys
+
 from model.database import DatabaseEngine
 from model.store import Store
 from exceptions import ResourceNotFound
 from model import *
-
+from PySide6.QtWidgets import QApplication
 from view.main_view import MainView
 
 
@@ -12,6 +14,7 @@ def main():
     # Init db
     database_engine = DatabaseEngine(url='sqlite:///shop.db')
     database_engine.create_database()
+    app = QApplication(sys.argv)
 
     # Database session is created when opening the app. All data will be commit in database at the end of the program.
     with database_engine.new_session() as db_session:
@@ -27,7 +30,7 @@ def main():
 
         try:
             # Run main view
-            MainView(store).show()
+            MainView()
         except KeyboardInterrupt:
             pass
         print("See you soon ! Bye !")
---
+++ view/__init__.py
@@ -0,0 +1,2 @@
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox
+from PySide6.QtGui import QCloseEvent

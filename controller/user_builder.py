54eec1fbf3bb5308a5d52272e5b8f5d1cedbbe8b
mathis kdio <cadio@et.esiea.fr>
2021-06-05 13:10:18 +0200

    update controller/user_builder.py

--- controller/user_builder.py
+++ controller/user_builder.py
@@ -1,11 +1,11 @@
-from model.mapping.customer import Customer
+from model.mapping.user import User
 from model.store import Store
 from controller.validation.user_validation import UserValidation
 
 
-class CustomerBuilder:
+class UserBuilder:
     """
-    customer create
+    user create
     """
 
     def __init__(self, store: Store):
@@ -14,13 +14,13 @@ class CustomerBuilder:
     def create_user(self, username: str, firstname: str, lastname: str, email: str, password: str, gender: str, age: str):
 
         # Save user in database
-        customer = Customer(username=username,
+        user = User(username=username,
                             firstname=firstname,
                             lastname=lastname,
                             email=email,
                             password=password,
                             gender=gender,
                             age=age)
-        UserValidation(customer).validate()
-        self._store.user().create(customer)
-        return customer
+        UserValidation(user).validate()
+        self._store.user().create(user)
+        return user

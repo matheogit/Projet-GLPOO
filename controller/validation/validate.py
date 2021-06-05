1d3619614daaede72591638764dea8fac94f5dc7
mathis kdio <cadio@et.esiea.fr>
2021-06-05 22:52:52 +0200

    add validate regex

--- controller/validation/validate.py
+++ controller/validation/validate.py
@@ -19,6 +19,18 @@ class Validate:
         email_pattern = "^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$"
         self._validate_regex(value, email_pattern)
 
+    def _validate_password(self, value):
+        email_pattern = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$"
+        self._validate_regex(value, email_pattern)
+
+    def _validate_gender(self, value):
+        email_pattern = "^[M|W]$"
+        self._validate_regex(value, email_pattern)
+
+    def _validate_age(self, value):
+        email_pattern = "^[0-9]{1,3}$"
+        self._validate_regex(value, email_pattern)
+
     def _validate_regex(self, value: str, regex: str):
         pattern = re.compile(regex)
         if isinstance(value, str) and not re.match(pattern, value):

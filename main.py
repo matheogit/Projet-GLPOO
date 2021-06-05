8853bcf010bfa398f6118702c9e12a68f96f0b2b
bastjog <bastienjoguet@gmail.com>
2021-06-05 14:28:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,30 +7,27 @@ from view.main_view import MainView
 
 
 def main():
-    print("## Welcome to the Shop ##\n")
-
-    # Init db
+   # Init db
     database_engine = DatabaseEngine(url='sqlite:///shop.db')
     database_engine.create_database()
 
     # Database session is created when opening the app. All data will be commit in database at the end of the program.
     with database_engine.new_session() as db_session:
         # Init store object
-        store = Store(db_session)
+        logiciel = Store(db_session)
 
         # Feed admin
         try:
-            store.user().get_by_username('admin')
+            logiciel.user().get_by_username('admin')
         except ResourceNotFound:
             admin = Admin(username="admin", firstname="admin", lastname="admin", email="contact@shop.fr", password="AZERTY", gender="admin", age="20")
             db_session.add(admin)
 
         try:
             # Run main view
-            MainView(store).show()
+            MainView(logiciel).show()
         except KeyboardInterrupt:
             pass
-        print("See you soon ! Bye !")
 
 
 if __name__ == "__main__":

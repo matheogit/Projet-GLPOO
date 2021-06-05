a2af0acbf6a12b086a80ac7ea58cbffb9fa5b316
matheogit <80523951+matheogit@users.noreply.github.com>
2021-06-05 14:19:06 +0200

    interface connection

---
+++ vue/connection/login.py
@@ -0,0 +1,58 @@
+from PySide6.QtWidgets import QVBoxLayout, QFormLayout, QLineEdit, QPushButton, QComboBox
+from vue.window import BasicWindow
+from vue.menu import MenuWindow
+
+
+class Login(BasicWindow):
+
+    def __init__(self):
+        #self._member_controller = member_controller
+        super().__init__()
+        ##
+
+        self.show_vue = None
+        self.window = None
+
+        self.username = QLineEdit()
+        self.password = QLineEdit()
+
+        self.window = None
+        self.setup()
+
+    def setup(self):
+        # Create an outer layout
+        outerLayout = QVBoxLayout()
+        # Create a form layout for the label and line edit
+        Layout = QFormLayout()
+        # Add a label and a line edit to the form layout
+
+        Layout.addRow("Utilisateur", self.username)
+
+        Layout.addRow("Mot de passe", self.password)
+
+        # Create a layout for the checkboxes
+        ValidationLayout = QVBoxLayout()
+
+        btn_login = QPushButton('Se connecter', self)
+        btn_login.clicked.connect(self.loginpage)
+        btn_login.resize(btn_login.sizeHint())
+        btn_login.move(90, 100)
+        ValidationLayout.addWidget(btn_login)
+        # Add some checkboxes to the layout
+        btn_cancel = QPushButton('Close', self)
+        btn_cancel.clicked.connect(self.quitEvent)
+        btn_cancel.resize(btn_cancel.sizeHint())
+        btn_cancel.move(90, 100)
+        ValidationLayout.addWidget(btn_cancel)
+        # Nest the inner layouts into the outer layout
+        outerLayout.addLayout(Layout)
+        outerLayout.addLayout(ValidationLayout)
+        # Set the window's main layout
+        self.setLayout(outerLayout)
+
+    def loginpage(self):
+        self.close()
+        self.window = MenuWindow()
+        self.window.show()
+
+
---
+++ vue/home.py
@@ -0,0 +1,46 @@
+from vue.window import BasicWindow
+from PySide6.QtWidgets import QApplication, QVBoxLayout, QPushButton
+from vue.connection.login import Login
+from vue.connection.register import Register
+
+
+class LoginWindow(BasicWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.window = None
+
+        self.setup()
+
+    def setup(self):
+
+        btn_login = QPushButton('Se connecter', self)
+        btn_login.clicked.connect(self.login)
+
+        btn_register = QPushButton('Créer un compte', self)
+        btn_register.clicked.connect(self.register)
+
+        btn_quit = QPushButton('Quitter', self)
+        btn_quit.clicked.connect(QApplication.instance().quit)
+
+        layout = QVBoxLayout()
+        layout.addWidget(btn_login)
+        layout.addWidget(btn_register)
+        layout.addWidget(btn_quit)
+
+        self.setGeometry(100, 100, 600, 600)
+        self.setWindowTitle('Page de connection')
+        self.setLayout(layout)
+        self.show()
+
+
+    def login(self):
+        #if self.window is None:
+        self.window = Login()
+        self.window.show()
+        test = 1
+
+    def register(self):
+        #if self.window is None:
+        self.window = Register()
+        self.window.show()

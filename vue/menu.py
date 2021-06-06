9d245aff47523ca968a709bf45aaa4ccefd54c92
mathis kdio <cadio@et.esiea.fr>
2021-06-06 14:25:51 +0200

    Amélioration du menu principal + fermeture de la fenêtre de connexion + déconnexion (#11)

--- vue/menu.py
+++ vue/menu.py
@@ -1,5 +1,5 @@
 from vue.window import BasicWindow
-from PySide6.QtWidgets import QApplication, QVBoxLayout, QPushButton
+from PySide6.QtWidgets import QLabel, QPushButton, QGridLayout, QWidget
 from vue.user.show import Party
 from vue.user.list import PartyList
 from vue.user.rank import PartyRank
@@ -10,7 +10,7 @@ class MenuWindow(BasicWindow):
 
     def __init__(self, user, store: Store):
         super().__init__()
-        self.window = None
+        self.window = QWidget()
         self._store = store
         self._user = user
 
@@ -19,6 +19,14 @@ class MenuWindow(BasicWindow):
         self.setStyleSheet("background-color: #B08AAD;")
 
     def setup(self):
+        self.setWindowTitle('Menu principal')
+
+        btn_profil = QPushButton('Profil', self)
+        #btn_profil.clicked.connect(self.)
+        btn_profil.setStyleSheet("background-color: #B6CFDF;")
+        
+        textProfil = QLabel('Bonjour, ' + self._user.username)
+
         btn_my = QPushButton('Mes soirées', self)
         btn_my.clicked.connect(self.my_party)
         btn_my.setStyleSheet("background-color: #B6CFDF;")
@@ -31,18 +39,20 @@ class MenuWindow(BasicWindow):
         btn_rank.clicked.connect(self.rank_party)
         btn_rank.setStyleSheet("background-color: #B6CFDF;")
 
-        btn_quit = QPushButton('Quitter', self)
-        btn_quit.clicked.connect(QApplication.instance().quit)
+        btn_quit = QPushButton('Déconnection', self)
+        btn_quit.clicked.connect(self.disconnect)
         btn_quit.setStyleSheet("background-color: #B6CFDF;")
 
-        layout = QVBoxLayout()
-        layout.addWidget(btn_my)
-        layout.addWidget(btn_list)
-        layout.addWidget(btn_rank)
-        layout.addWidget(btn_quit)
+        layout = QGridLayout()
+        layout.addWidget(btn_profil, 0, 0, 1, 1)
+        layout.addWidget(textProfil, 0, 3, 1, 1)
+        layout.addWidget(btn_my, 1, 1, 1, 1)
+        layout.addWidget(btn_list, 2, 1, 1, 1)
+        layout.addWidget(btn_rank, 3, 1, 1, 1)
+        layout.addWidget(btn_quit, 4, 1, 1, 1)
+        layout.setRowStretch(4, 1)
 
         self.setGeometry(100, 100, 600, 600)
-        self.setWindowTitle('Menu des soirées')
         self.setLayout(layout)
         self.show()
 
@@ -63,3 +73,9 @@ class MenuWindow(BasicWindow):
         # if self.window is None:
         self.window = PartyRank()
         self.window.show()
+
+    def disconnect(self):
+        from vue.home import LoginWindow
+        self.window = LoginWindow(self._store)
+        self.window.show()
+        self.close()
